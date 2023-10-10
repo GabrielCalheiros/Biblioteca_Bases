db75e19e8e6450a5b0d26b12f369d89d4a750b64
GabrielCalheiros <gabrielbcm.1998@gmail.com>
2023-10-10 11:04:22 -0300

    agora sim

---
+++ infoGraphics/lists.js
@@ -0,0 +1,49 @@
+const ListsCreated = {"Conspiracy" : [
+  "Gatekeepers Remorse.jpg",
+  "pizzagate1.jpg",
+  "pizzagate2.jpg",
+  "Textbooks Of The Elite.png",
+  "x.jpg"
+],
+"History" : [
+  "Bronze Age.jpg",
+  "Greeks - 1.jpg",
+  "Greeks - 2.jpg",
+  "Romans - 1.jpg",
+  "Romans - 2.jpg"
+],
+"Literature" : [
+  "Dark and Disturbing Literature.jpg",
+  "Greek Literature.jpg",
+  "Horror Fiction.jpg",
+  "Lit Favorite Poets.png",
+  "Lit Top Books.jpg",
+  "Lit Top Novels.jpg",
+  "Literatura Portugues.png",
+  "Lovecraft.png",
+  "Patrician Core.jpg",
+  "St Johns College Great Books List.png"
+],
+"Politics" : [
+  "Facism.jpg",
+  "Julius Evola.jpg",
+  "Slave Lord Guide.jpg",
+  "Ted Kaczynski Core.png"
+],
+"Religion" : [
+  "Buddhism 1.jpg",
+  "Buddhism 2.jpg",
+  "Christianity - Mysticism.jpg",
+  "Christianity - Orthodox.jpg",
+  "Christianity.png",
+  "Hermeticism.jpg",
+  "Initiation into Hermetics.png",
+  "Islam.jpg",
+  "Medieval Mysticism.png",
+  "Western Occultism - 1.png",
+  "Western Occultism - 2.png",
+  "World Religions Chart.jpg"
+],
+"Science" : [
+  "Computer Science.jpg"
+]};
