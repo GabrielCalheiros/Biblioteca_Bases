dc26716a06efced92350485f9fbf720c91bdfc3b
GabrielCalheiros <gabrielbcm.1998@gmail.com>
2023-10-10 17:42:58 -0300

    terça a tarde nao sei oq fiz

--- infoGraphics/lists.js
+++ infoGraphics/lists.js
@@ -12,19 +12,32 @@ const ListsCreated = {"Conspiracy" : [
   "Romans - 1.jpg",
   "Romans - 2.jpg"
 ],
+"Kommando" : [
+  "Kommando.png",
+  "Victory Now.jpg"
+],
 "Literature" : [
   "Dark and Disturbing Literature.jpg",
   "Greek Literature.jpg",
   "Horror Fiction.jpg",
+  "Latin Literature.jpg",
   "Lit Favorite Poets.png",
   "Lit Top Books.jpg",
   "Lit Top Novels.jpg",
   "Literatura Portugues.png",
   "Lovecraft.png",
   "Patrician Core.jpg",
+  "SciFi.jpg",
   "St Johns College Great Books List.png"
 ],
+"Literature by Country" : [
+  "Indian.jpg",
+  "Iran.png",
+  "Irish.png",
+  "Korean.jpg"
+],
 "Politics" : [
+  "Communalism.png",
   "Facism.jpg",
   "Julius Evola.jpg",
   "Slave Lord Guide.jpg",
@@ -40,10 +53,14 @@ const ListsCreated = {"Conspiracy" : [
   "Initiation into Hermetics.png",
   "Islam.jpg",
   "Medieval Mysticism.png",
+  "Satanism.png",
   "Western Occultism - 1.png",
   "Western Occultism - 2.png",
   "World Religions Chart.jpg"
 ],
 "Science" : [
-  "Computer Science.jpg"
+  "Computer Science.jpg",
+  "Math - 1.jpg",
+  "Math - 2.png",
+  "Trivium.png"
 ]};
