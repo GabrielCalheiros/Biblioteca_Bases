a48e70a7efcc885b5b9d98caa6d4f99513b9b759
GabrielCalheiros <gabrielbcm.1998@gmail.com>
2023-10-11 10:12:30 -0300

    minor changes

--- infoGraphics/lists.js
+++ infoGraphics/lists.js
@@ -1,58 +1,121 @@
-const ListsCreated = {"Conspiracy" : [
+const ListsCreated = {"Authors" : [
+  "Borges.jpg",
+  "Dostoyevsky - Translations Comparison.jpg",
+  "Julius Evola.jpg",
+  "Lovecraft.png",
+  "Nietzsche - 1.png",
+  "Nietzsche - 2.png",
+  "Rene Guenon.png",
+  "Ted Kaczynski.png",
+  "Zizek - 1.jpg",
+  "Zizek - 2.png"
+],
+"Canon" : [
+  "St Johns College Great Books List.png",
+  "The Lit Canon.jpg"
+],
+"Conspiracy" : [
   "Gatekeepers Remorse.jpg",
   "pizzagate1.jpg",
   "pizzagate2.jpg",
   "Textbooks Of The Elite.png",
   "x.jpg"
 ],
-"History" : [
-  "Bronze Age.jpg",
-  "Greeks - 1.jpg",
-  "Greeks - 2.jpg",
-  "Romans - 1.jpg",
-  "Romans - 2.jpg"
-],
-"Kommando" : [
-  "Kommando.png",
-  "Victory Now.jpg"
-],
-"Literature" : [
-  "Dark and Disturbing Literature.jpg",
-  "Greek Literature.jpg",
-  "Horror Fiction.jpg",
-  "Latin Literature.jpg",
-  "Lit Favorite Poets.png",
-  "Lit Top Books.jpg",
-  "Lit Top Novels.jpg",
-  "Literatura Portugues.png",
-  "Lovecraft.png",
-  "Patrician Core.jpg",
-  "SciFi.jpg",
-  "St Johns College Great Books List.png"
-],
-"Literature by Country" : [
+"Country & Region" : [
+  "Brazil - Comtemporanea.png",
+  "Brazil - Modernista.png",
+  "Brazil - Pos Romanticos.png",
+  "Canada - Authors.png",
+  "Canada - Poetry.png",
+  "China.jpg",
+  "France - Forgotten.jpg",
+  "France - Renaissance.png",
   "Indian.jpg",
   "Iran.png",
   "Irish.png",
-  "Korean.jpg"
+  "Italy -Renaissance Literature.png",
+  "Italy.jpg",
+  "Korean.jpg",
+  "Latin America.png",
+  "Norway.png",
+  "Poland.png",
+  "Serbia.png",
+  "Spain - Mystic.png",
+  "Spain.png"
+],
+"History, Geography & Antropology" : [
+  "Bronze Age.jpg",
+  "Greeks - 1.jpg",
+  "Greeks - 2.jpg",
+  "Greeks - 3.jpg",
+  "Indo Europeans.jpg",
+  "Romans - 1.jpg",
+  "Romans - 2.jpg",
+  "Romans - 4.jpg"
+],
+"Horror, Creepy & Disturbing" : [
+  "Dark and Disturbing Literature.jpg",
+  "Gothic and Horror Flowchart.jpg",
+  "Horror Fiction.jpg",
+  "Horror Story Collections.png",
+  "Weird Stories.png"
+],
+"Kommando" : [
+  "Fitchart.jpg",
+  "Kommando.png",
+  "Victory Now.jpg",
+  "War Literature.jpg"
+],
+"Literature" : [
+  "Cyberpunk.png",
+  "Fantasy.jpg",
+  "Literatura Portugues.png",
+  "National Epics.png",
+  "Patrician Core.jpg",
+  "SciFi.jpg",
+  "Short Story.jpg",
+  "Web Novels.jpg",
+  "Western Theatre.png"
+],
+"Philosophy" : [
+  "Legal_Philosophy_Guide.png"
+],
+"Poetry" : [
+  "Lit Favorite Poets.png",
+  "Poems on Massacres and Hell.jpg",
+  "Poetry - Meme Edition.jpg"
 ],
 "Politics" : [
+  "4chan Pol - 1.jpg",
+  "4chan Pol - 2.jpg",
+  "British Conservatism.jpg",
   "Communalism.png",
+  "Distributism.jpg",
+  "EcoFascism.png",
   "Facism.jpg",
-  "Julius Evola.jpg",
-  "Slave Lord Guide.jpg",
-  "Ted Kaczynski Core.png"
+  "Leftist & Progressive.jpg",
+  "Monarchia.jpg",
+  "National Communism.jpg",
+  "Neoliberal.png",
+  "Right Wing Socialism.png",
+  "Slave Lord Guide.jpg"
 ],
 "Religion" : [
+  "Bibles Translations.png",
   "Buddhism 1.jpg",
   "Buddhism 2.jpg",
+  "Celtic Paganism.png",
+  "Christianity - Ecotheology.jpg",
   "Christianity - Mysticism.jpg",
-  "Christianity - Orthodox.jpg",
+  "Christianity - Orthodox 1.jpg",
+  "Christianity - Orthodox 2 PLUS SCHIZO STUFF.png",
   "Christianity.png",
   "Hermeticism.jpg",
+  "Hindu Texts.png",
   "Initiation into Hermetics.png",
   "Islam.jpg",
   "Medieval Mysticism.png",
+  "Quran Translations.jpg",
   "Satanism.png",
   "Western Occultism - 1.png",
   "Western Occultism - 2.png",
@@ -62,5 +125,28 @@ const ListsCreated = {"Conspiracy" : [
   "Computer Science.jpg",
   "Math - 1.jpg",
   "Math - 2.png",
+  "Math - 3.jpg",
+  "Math - 4.jpg",
   "Trivium.png"
+],
+"Themed Charts" : [
+  "Bioshock Infinite.jpg",
+  "Books Which Make You Happier.png",
+  "Burgerpunk.png",
+  "Conan The Barbarian.jpg",
+  "Drooger.png",
+  "Evangelion Further Reading.png",
+  "Great Characters.jpg",
+  "Lain Pill.jpg",
+  "Outdoors.png",
+  "Paranoia.png",
+  "Pewdiepie Reading List.jpg",
+  "Warhammer 40k - 1.jpg",
+  "Warhammer 40k - 2.jpg"
+],
+"Tops" : [
+  "Lit Top 100 Books 2018.jpg",
+  "Lit Top 100 Books 2019.jpg",
+  "Lit Top 100 Books 2020.jpg",
+  "Lit Top 100 Novels 2020.jpg"
 ]};
